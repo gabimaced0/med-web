fc4047f0b236bb483a8e9da45577b997ef5ef0fc
Gabrielly Macedo <162237578+gabimaced0@users.noreply.github.com>
2025-04-01 21:38:11 -0300

    ajuste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import AppointmentItem from "@/components/appointment-item";
 import NavBar from "@/components/nav-bar";
-import { appRouterContext } from "next/dist/server/route-modules/app-route/shared-modules";
 
 
 export default function AppointmentPage() {
@@ -11,9 +9,7 @@ export default function AppointmentPage() {
       <main className="flex justify-center">
         <div className="bg-sky-900 p6 m-6 rounded min-w-1/3">
           <h2>Agendamentos</h2>
-          <AppointmentItem/>
-          <AppointmentItem/>
-          <AppointmentItem/>
+          
 
         </div>
 
